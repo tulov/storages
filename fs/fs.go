55f1ce6d32f4a5ef73628971c17b40c887bab261
tulov.alex <tulov.alex@gmail.com>
2021-09-22 01:19:54 -0700

    Storages for picfit

---
+++ fs/fs.go
@@ -0,0 +1,77 @@
+package fs
+
+import (
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/tulov/storages"
+)
+
+// Storage is a filesystem storage.
+type Storage struct {
+	root string
+}
+
+// NewStorage returns a new filesystem storage.
+func NewStorage(cfg Config) *Storage {
+	return &Storage{root: cfg.Root}
+}
+
+// Config is the configuration for Storage.
+type Config struct {
+	Root string
+}
+
+func (fs *Storage) abs(path string) string {
+	return filepath.Join(fs.root, path)
+}
+
+// Save saves content to path.
+func (fs *Storage) Save(_ context.Context, content io.Reader, path string) error {
+	abs := fs.abs(path)
+	if err := os.MkdirAll(filepath.Dir(abs), 0755); err != nil {
+		return err
+	}
+
+	w, err := os.Create(abs)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	if _, err := io.Copy(w, content); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Stat returns path metadata.
+func (fs *Storage) Stat(_ context.Context, path string) (*storages.Stat, error) {
+	fi, err := os.Stat(fs.abs(path))
+	if os.IsNotExist(err) {
+		return nil, storages.ErrNotExist
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &storages.Stat{
+		ModifiedTime: fi.ModTime(),
+		Size:         fi.Size(),
+	}, nil
+}
+
+// Open opens path for reading.
+func (fs *Storage) Open(_ context.Context, path string) (io.ReadCloser, error) {
+	f, err := os.Open(fs.abs(path))
+	if os.IsNotExist(err) {
+		return nil, storages.ErrNotExist
+	}
+	return f, err
+}
+
+// Delete deletes path.
+func (fs *Storage) Delete(_ context.Context, path string) error {
+	return os.Remove(fs.abs(path))
+}
---
+++ s3/s3.go
@@ -0,0 +1,149 @@
+package s3
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go"
+	"github.com/aws/smithy-go/transport/http"
+	"github.com/tulov/storages"
+	"io"
+	"log"
+	"mime"
+	"os"
+	"path/filepath"
+)
+
+// Storage is a s3 storage.
+type Storage struct {
+	bucket string
+	s3     *s3.Client
+}
+
+// Config is the configuration for Storage.
+type Config struct {
+	Bucket      string
+	EndpointUrl string
+}
+
+func NewStorage(cfg Config) (*Storage, error) {
+	var awsConfig aws.Config
+	var err error
+
+	if cfg.EndpointUrl != "" {
+		customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+			return aws.Endpoint{
+				PartitionID:   "s3",
+				URL:           cfg.EndpointUrl,
+				SigningRegion: os.Getenv("AWS_REGION"),
+			}, nil
+		})
+
+		awsConfig, err = config.LoadDefaultConfig(
+			context.TODO(),
+			config.WithEndpointResolver(customResolver))
+
+	} else {
+		awsConfig, err = config.LoadDefaultConfig(context.TODO())
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(awsConfig)
+	return &Storage{
+		bucket: cfg.Bucket,
+		s3:     client,
+	}, nil
+}
+
+// Save saves content to path.
+func (s *Storage) Save(ctx context.Context, content io.Reader, path string) error {
+	var input *s3.PutObjectInput
+	if _, ok := content.(io.Seeker); !ok {
+		b, err := io.ReadAll(content)
+		if err != nil {
+			return err
+		}
+		input = &s3.PutObjectInput{
+			Bucket:        aws.String(s.bucket),
+			Key:           aws.String(path),
+			Body:          bytes.NewBuffer(b),
+			ContentLength: int64(len(b)),
+		}
+		_, err = s.s3.PutObject(ctx, input,
+			s3.WithAPIOptions(
+				v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware,
+			))
+		return err
+	}
+	input = &s3.PutObjectInput{
+		Bucket:      aws.String(s.bucket),
+		Key:         aws.String(path),
+		Body:        content,
+		ContentType: aws.String(mime.TypeByExtension(filepath.Ext(path))),
+	}
+
+	_, err := s.s3.PutObject(ctx, input)
+	return err
+}
+
+// Stat returns path metadata.
+func (s *Storage) Stat(ctx context.Context, path string) (*storages.Stat, error) {
+	input := &s3.HeadObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(path),
+	}
+	out, err := s.s3.HeadObject(ctx, input)
+	if err != nil {
+		var re *http.ResponseError
+		if errors.As(err, &re) {
+			var gae *smithy.GenericAPIError
+			if errors.As(re.Err, &gae) && gae.Code == "NotFound" {
+				return nil, storages.ErrNotExist
+			}
+		}
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &storages.Stat{
+		ModifiedTime: *out.LastModified,
+		Size:         out.ContentLength,
+	}, nil
+}
+
+// Open opens path for reading.
+func (s *Storage) Open(ctx context.Context, path string) (io.ReadCloser, error) {
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(path),
+	}
+	out, err := s.s3.GetObject(ctx, input)
+	if err != nil {
+		var re *http.ResponseError
+		if errors.As(err, &re) {
+			var gae *smithy.GenericAPIError
+			if errors.As(re.Err, &gae) && gae.Code == "NoSuchKey" {
+				return nil, storages.ErrNotExist
+			}
+		}
+	} else if err != nil {
+		return nil, err
+	}
+	return out.Body, nil
+}
+
+// Delete deletes path.
+func (s *Storage) Delete(ctx context.Context, path string) error {
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(path),
+	}
+	_, err := s.s3.DeleteObject(ctx, input)
+	return err
+}
